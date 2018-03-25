837890847301322b43b1adc5a683c6bf94f5091d
Sourabh Ranka <sranka29@rediffmail.com>
2018-03-25 05:09:30 -0700

    Typescritp basics

---
+++ basics.js
@@ -0,0 +1,3 @@
+console.log('Hi !! I am in typescript');
+var count = 3;
+var name = 'Sourabh';
---
+++ basics.ts
@@ -0,0 +1,33 @@
+console.log('Hi !! I am in typescript');
+
+let count : Number = 3;
+let name : string = 'Sourabh'; // how to create vaiables in typescript
+
+class myClass{ // how to create classes
+
+    var1 = 3; // how to create property of classes
+
+    myFunction(){ // how to create function within class
+        console.log('this is my function');
+    }
+
+}
+
+interface myInterface{ // how to create interfaces
+
+    age : Number;
+    number : string ;
+
+
+}
+
+
+function abc(){ // normal function without any parameter and single statement
+
+    return 'something';
+
+}
+
+
+abc => 'something'; // equivalent arrow function
+
